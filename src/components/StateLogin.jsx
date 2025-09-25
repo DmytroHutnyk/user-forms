c4fd79f3f0bce99b827e995a76907de541f69532
Dmytro Hutnyk <dgutnik1@gmail.com>
2025-09-25 14:42:53 +0200

    feat: add form validation on every keystroke

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,8 +1,12 @@
 import {useState} from "react";
 
-export default function UseStateLogin() {
-    const [form, setForm] = useState({ email: "", password: "" });
+export default function StateLogin() {
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    });
 
+    const emailIsInvalid = form.email !== '' && !form.email.includes('@');
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -33,6 +37,7 @@ export default function UseStateLogin() {
                         value={form.email}
                         onChange={handleChange} />
                 </div>
+                <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
 
                 <div className="control no-margin">
                     <label htmlFor="password">Password</label>
