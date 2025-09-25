881c72aac46d1eb5e9a808200fc7ae6cdbd87b9d
Dmytro Hutnyk <dgutnik1@gmail.com>
2025-09-25 15:26:13 +0200

    feat: add form validation on submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
-import {useRef} from "react";
+import {useRef, useState} from "react";
 
 export default function Login() {
     const email = useRef();
     const password = useRef();
+
+    const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -10,7 +12,13 @@ export default function Login() {
         const enteredEmail = email.current.value;
         const enteredPassword = password.current.value;
 
-        console.log(enteredEmail + ", " + enteredPassword);
+        const emailInvalid = !enteredEmail.includes('@');
+        if(emailInvalid){
+            setEmailIsInvalid(true);
+            return;
+        }
+
+        console.log("Sending HTTP request ....");
     }
 
   return (
@@ -25,6 +33,7 @@ export default function Login() {
               name="email"
               type="email"
               ref={email} />
+            <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
         </div>
 
         <div className="control no-margin">
